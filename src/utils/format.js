c7b3ed9716363e135677f677f0501d7685782b5c
Eduardo Quintero <eduardoquintero@192.168.1.12>
2022-03-16 22:29:22 -0700

    Removed isEmpty function

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,8 +1,1 @@
 export const twoDigitsInteger = number => (number <= 0 ? '00' : number);
-
-export const isEmpty = object => {
-  for (const property in object) {
-    return false;
-  }
-  return true;
-};
